a3cc02cbf612c93703ec4f0a87b32828779a0182
Samiur Rahman Rony <samiur.rahman97@gmail.com>
2023-04-01 01:50:24 +0600

    validation with required

--- form_validation.php
+++ form_validation.php
@@ -1,3 +1,54 @@
+<?php  
+$errname = $erremail = $errmbl = $errgender = "";
+$gender = $name = $email = $mbl =  "";
+            
+
+if ($_SERVER['REQUEST_METHOD']== "POST") {
+
+    if(empty($_POST["name"])){
+        $errname = "<span style = 'color:red'>Name is required.</span>";
+    }
+    else {
+        $name = validation($_POST["name"]);
+    }
+
+    if(empty($_POST["email"])){
+        $erremail = "<span style = 'color:red'>Email is required.</span>";
+    }
+    else {
+        $email = validation($_POST["email"]);
+    }
+
+    if(empty($_POST["mbl"])){
+        $errmbl = "<span style = 'color:red'>Mobile number is required.</span>";
+    }
+    else {
+        $mbl = validation($_POST["mbl"]);
+    }
+
+    if(empty($_POST["gender"])){
+        $errgender = "<span style = 'color:red'>Gender is required.</span>";
+    }
+    else {
+        $gender = validation($_POST["gender"]);
+    }
+
+    $comment = validation($_POST["comment"]);
+
+
+    
+
+}
+function validation($data){
+    $data1 = trim($data);
+    $data2 = stripcslashes($data1);
+    $datafinal = htmlspecialchars($data2);
+
+    return $datafinal;
+}
+
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -18,73 +69,57 @@
       </section>
 
         <section class="maincontent">
+
         <section class= "contentHeading">
                 <hr>Topics-> Form Validation:<hr>
         </section>
 
-    <form method= "post" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
-
-        <table>
-            <tr>
-                <th>Name:</th>
-                <td><input type="text" name="name" required /></td>
-            </tr>
-            <tr>
-                <th>Email:</th>
-                <td><input type="email" name="email" required /></td>
-            </tr>
-            <tr>
-                <th>Phone Number:</th>
-                <td><input type="text" name="mbl" required /></td>
-            </tr>
-            <tr>
-                <th>Comment:</th>
-                <td><textarea name="comment" rows="5" cols="40" ></textarea></td>
-            </tr>
-            <tr>
-                <th>Gender:</th>
-                <td>
-                    <input type="radio" name="gender" value= "male" required/> Male
-                    <input type="radio" name="gender" value= "female" required/> Female
-                </td>
-            </tr>
-            <tr>
-                
-                <td>
-                    <input type="submit" name="submit" value= "submit"/>
-                    
-                </td>
-            </tr>
-        </table>
-    </form>
-
         <section>
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] == "POST") {
-                $name = validation($_POST['name']);
-                $email = validation($_POST['email']);
-                $mbl = validation($_POST['mbl']);
-                $comment = validation($_POST['comment']);
-                $gender = validation($_POST['gender']);
+        <form method= "post" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
 
-                echo "Name: ". $name . "</br>";
-                echo "Email: ". $email . "</br>";
-                echo "Mobile Number: ". $mbl . "</br>";
-                echo "comment: ". $comment . "</br>";
-                echo "gender: ". $gender;
-
-            }
-            function validation($data){
-                $data1 = trim($data);
-                $data2 = stripcslashes($data1);
-                $datafinal = htmlspecialchars($data2);
-
-                return $datafinal;
-            }
-           
-             
-            ?>
-
+<table>
+<p style = "color:red"> Required field </p>
+    <tr>
+        <th>Name:</th>
+        <td><input type="text" name="name"  />*<?php echo $errname; ?></td>
+    </tr>
+    <tr>
+        <th>Email:</th>
+        <td><input type="email" name="email"  />*<?php echo $erremail; ?></td>
+    </tr>
+    <tr>
+        <th>Phone Number:</th>
+        <td><input type="text" name="mbl"  />*<?php echo $errmbl; ?></td>
+    </tr>
+    <tr>
+        <th>Comment:</th>
+        <td><textarea name="comment" rows="5" cols="40" ></textarea></td>
+    </tr>
+    <tr>
+        <th>Gender:</th>
+        <td>
+            <input type="radio" name="gender" value= "male" /> Male
+            <input type="radio" name="gender" value= "female" /> Female
+            *<?php echo $errgender; ?>
+        </td>
+    </tr>
+    <tr>
+        
+        <td>
+            <input type="submit" name="submit" value= "submit"/>
+            
+        </td>
+    </tr>
+</table>
+</form>     
+        <?php
+            echo "Name: ". $name . "</br>";
+            echo "Email: ". $email . "</br>";
+            echo "Mobile Number: ". $mbl . "</br>";
+            echo "Comment: ". $comment . "</br>";
+            echo "Gender: ". $gender;
+            
+        ?>
         </section>
         </section>
         <section class="footer">
